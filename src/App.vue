752dcf668f1d14f6728cb5326e93e47de072782e
Rafael Bar <rafaelbar37>
2020-03-26 17:27:42 +0200

    al the basic of vuex

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,14 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Vuex</h1>
-                <app-result :counter="counter"></app-result>
+                <app-result></app-result>
+                <app-another-result></app-another-result>
                 <hr>
-                <app-counter @updated="counter += $event"></app-counter>
+                <app-counter></app-counter>
+                <app-another-counter></app-another-counter>
+                <hr>
+                <input type="text" class="" v-model="value">
+                <p class="">{{value}}</p>
             </div>
         </div>
     </div>
@@ -14,17 +19,32 @@
 <script>
     import Counter from './components/Counter.vue';
     import Result from './components/Result.vue';
+    import AnotherResult from './components/AnotherResult.vue';
+    import AnotherCounter from './components/AnotherCounter.vue';
 
     export default {
-        data() {
-            return {
-                counter: 0
+        computed: {
+            value: {
+                get(){
+                    return this.$store.getters.value;
+                },
+                set(value){
+                    this.$store.dispatch('updateValue', value);
+                }
+            }
+        },
+        methods: {
+            updateValue(event) {
+                return this.$store.dispatch('updateValue', event.target.value);
             }
         },
         components: {
             appCounter: Counter,
             appResult: Result,
+            appAnotherResult: AnotherResult,
+            appAnotherCounter: AnotherCounter
         }
     }
 </script>
+
 
